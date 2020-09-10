c77f2585a4abc2e376378bfb0bc92e328ca26c84
Takasu <m01191694a@edu.teu.ac.jp>
2020-09-10 18:33:57 +0900

    C#データ追加（主人公移動・１人称視点）

---
+++ Assets/Script/Xcamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xcamera : MonoBehaviour
+{
+    [SerializeField]
+    private float x_sensitivity = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x_mouse = Input.GetAxis("Mouse X");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += x_mouse * x_sensitivity;
+        transform.localEulerAngles = newRotation;
+    }
+}
